b18900a04b6ecb4145718a4062994551fbf52237
AdityaG028393 <90874554+AdityaG028393@users.noreply.github.com>
2022-08-15 18:51:20 -0500

    Add files via upload

---
+++ screens/CreateStory.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react';
+import {Text,View} from 'react-native';
+
+export default class CreaetStory extends Component{
+    render(){
+        return(
+            <View
+            
+            style={{
+                flex:1,
+                justidyContent:"center",
+                alignItems:"center",
+            }}
+            
+            >
+               
+                <ET
+            </View>
+        )
+    }
+}
